3ab737fef0ff1e6fd8e31172c4ae22d69c6a0b6b
Ankita-shrivastav <93003257+Ankita-shrivastav@users.noreply.github.com>
2023-12-22 02:14:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 //your JS code here. If required.
+var browserInfo="You are using " + navigator.appName+ "version" + navigator.appVersion;
+document.getElementById("browser-info").innerText = browserInfo;
